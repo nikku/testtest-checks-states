f55b54073b598cfd0e35cccf66b68a27ade9ab80
Nico Rehwaldt <git_nikku@nixis.de>
2018-11-29 11:15:55 +0100

    feat: break coverage

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
 module.exports = function() {
-  return 1 + 1;
+  if (true) {
+    return 1 + 1
+  }
+
+  return [
+    1,
+    2,
+    3,
+    4
+  ].reduce(function(sum, i) {
+    return sum + i;
+  }, 0);
 };
